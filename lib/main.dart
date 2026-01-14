c406581582bfa08e0e42502fa7893e41cf1b036c
Pratik <prtkgsh003@gmail.com>
2026-01-14 14:23:53 +0530

    Initial Provider counter app

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:counter_app/providers/counter_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => CounterProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage()
+    );
+  }
+}
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final counter = context.watch<CounterProvider>().count;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Provider Counter'),
+      ),
+      body: Center(
+        child: Text(
+          '$counter',
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            heroTag: 'inc',
+            onPressed: () {
+              context.read<CounterProvider>().increment();
+            },
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            heroTag: 'dec',
+            onPressed: () {
+              context.read<CounterProvider>().decrement();
+            },
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
