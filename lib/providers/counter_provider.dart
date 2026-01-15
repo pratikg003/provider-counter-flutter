7edbc2e2e418d91a9dd4d6a6c11983728fb0f386
Pratik <prtkgsh003@gmail.com>
2026-01-15 13:39:42 +0530

    Day 32: Provider with persistence

--- lib/providers/counter_provider.dart
+++ lib/providers/counter_provider.dart
@@ -1,22 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CounterProvider extends ChangeNotifier{
   int _count = 0;
 
   int get count => _count;
 
+  CounterProvider(){
+    _loadCount();
+  }
+
+  Future<void> _loadCount() async{
+    final prefs = await SharedPreferences.getInstance();
+    _count = prefs.getInt('count') ?? 0;
+    notifyListeners();
+  }
+  Future<void> _saveCount() async{
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('count', _count);
+  }
+
   void increment(){
     _count++;
+    _saveCount();
     notifyListeners();
   }
 
   void decrement(){
     _count--;
+    _saveCount();
     notifyListeners();
   }
 
   void reset(){
     _count = 0;
+    _saveCount();
     notifyListeners();
   }
 }
